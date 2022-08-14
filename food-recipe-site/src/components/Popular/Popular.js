3cfd4f7f846f44c16b42321319dec118897dc9f9
Ayşenur Kamiloğlu <77462852+aysenurkamiloglu@users.noreply.github.com>
2022-08-15 02:33:17 +0300

    Update Popular.js

--- food-recipe-site/src/components/Popular/Popular.js
+++ food-recipe-site/src/components/Popular/Popular.js
@@ -22,7 +22,7 @@ const Popular = () => {
         if (check) {
             setPopular(JSON.parse(check));
         } else {
-            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=6`);
+            const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
             const data = await api.json();
 
             localStorage.setItem("popular", JSON.stringify(data.recipes));
@@ -35,7 +35,7 @@ const Popular = () => {
             <div className={style.popular}>
                 <h3> Most Searched Foods </h3>
                 <Splide options={{
-                    perPage: 3,
+                    perPage: 5,
                     drag: "free",
                     gap: "5rem",
                 }}>
