5a8ce734f36915d3861350ba2e0dd98167599017
Aysenur <aysenurkamiloglu@outlook.com>
2022-08-14 13:08:25 +0300

    gluten free ve ketogenic home page'e eklendi

--- food-recipe-site/src/pages/Home.js
+++ food-recipe-site/src/pages/Home.js
@@ -1,12 +1,17 @@
 import Popular from "../components/Popular/Popular";
 import Vegan from "../components/Vegan/Vegan";
+import Ketogenic from "../components/Ketogenic/Ketogenic";
+
 import {motion} from "framer-motion";
+import GlutenFree from "../components/GlutenFree/GlutenFree";
 
 const Home = () => {
     return(
         <motion.div animate={{opacity: 1}} initial={{opacity: 0}} exit={{opacity: 0}} transition={{duration: 0.5}}>
             <Popular></Popular>
             <Vegan></Vegan>
+            <Ketogenic></Ketogenic>
+            <GlutenFree></GlutenFree>
         </motion.div>
     )
 }
