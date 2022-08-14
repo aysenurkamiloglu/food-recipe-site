d645c3e4acbc66ca160eb1644932687fadff977d
Aysenur <aysenurkamiloglu@outlook.com>
2022-08-14 12:25:58 +0300

    tüm sayfaların routelama işlemi yapıldı

--- food-recipe-site/src/pages/Pages.js
+++ food-recipe-site/src/pages/Pages.js
@@ -1,11 +1,22 @@
 import Home from "./Home";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Cuisine from "./CuisinePages/Cuisine";
+import Searched from "./Searched/Searched";
+import RecipeDetail from "./RecipeDetail/RecipeDetail";
+import { AnimatePresence } from "framer-motion";
 
 
 const Pages = () => {
-    return(
-        <div>
-            <Home></Home>
-        </div>
+    const location = useLocation();
+    return (
+        <AnimatePresence exitBeforeEnter>
+            <Routes location={location} key={location.pathname}>
+                <Route path="/" element={<Home />} />
+                <Route path="/cuisine/:type" element={<Cuisine />} />
+                <Route path="/searched/:search" element={<Searched />} />
+                <Route path="/recipe/:recipeName" element={<RecipeDetail />} />
+            </Routes>
+        </AnimatePresence>
     )
 }
 
