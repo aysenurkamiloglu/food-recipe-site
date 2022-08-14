3b914786680084b0416b1c59f7ebde1bb0a6366e
Aysenur <aysenurkamiloglu@outlook.com>
2022-08-14 12:26:39 +0300

    App.js düzenlendi

--- food-recipe-site/src/App.js
+++ food-recipe-site/src/App.js
@@ -1,11 +1,24 @@
+import Categories from "./components/Categories/Categories";
 import Pages from "./pages/Pages";
+import { BrowserRouter } from "react-router-dom";
+import SearchBox from "./components/SearchBox/SearchBox";
+import { Link } from "react-router-dom";
+import { GiChefToque } from "react-icons/gi";
+import style from "./App.module.scss";
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Merhabaaalar</h1>
-      <Pages></Pages>
+    <div>
+      <BrowserRouter>
+        <div className={style.header}>
+          <GiChefToque className={style.logo}/>
+          <Link className={style.logo} to={"/"}>yummy</Link>
+        </div>
+        <SearchBox></SearchBox>
+        <Categories></Categories>
+        <Pages></Pages>
+      </BrowserRouter>
     </div>
   );
 }
